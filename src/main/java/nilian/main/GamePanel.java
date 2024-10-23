7aae96ffe0aa59d6520ef5587abb3e8bd9b36075
Nilian <scaleenor@gmail.com>
2024-10-23 18:50:31 +0330

    [UPDATE] Made the Map and the window a little bit bigger

--- src/main/java/nilian/main/GamePanel.java
+++ src/main/java/nilian/main/GamePanel.java
@@ -17,8 +17,8 @@ public class GamePanel extends JPanel implements Runnable
 	public final int originalTileSize = 16 ;
 	public final int scale = 3 ;
 	public final int tileSize = originalTileSize * scale ;//16 * 3 = 48 x48 tile
-	public final int maxScreenCol = 16 ;
-	public final int maxScreenRow = 12 ;
+	public final int maxScreenCol = 24 ;
+	public final int maxScreenRow = 14 ;
 	public final int screenWidth = tileSize * maxScreenCol ;//768 pixels
 	public final int screenHeight = tileSize * maxScreenRow ;//576 pixels
 	

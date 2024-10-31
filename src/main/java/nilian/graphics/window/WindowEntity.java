774814ab4a3f666f7f0ddd6626f04d691be5354a
smh_tabatabaei <tabatabei.smh@yaftar.ir>
2024-10-31 12:52:18 +0330

    [RENAME] resources/image to resources/background

--- src/main/java/nilian/graphics/window/WindowEntity.java
+++ src/main/java/nilian/graphics/window/WindowEntity.java
@@ -2,7 +2,7 @@ package nilian.graphics.window;
 
 public abstract class WindowEntity {
 
-    public static final String BACKGROUND_IMAGE_PATH = "/images/dragon.jpg";
+    public static final String BACKGROUND_IMAGE_PATH = "/backgrounds/dragon.jpg";
     public static final String WINDOW_TITLE = "Wizard: The Beginning Of Story";
     public static final int TEXT_FIELD_COLUMNS = 20;
 
